be7d508ed609f1c2e1adbad9b6206bf575512cc5
Rubiksron <rubiksron@gmail.com>
2019-05-28 12:09:55 -0700

    added some feature to clear results div, and cleaned up some code.

--- public/api.js
+++ public/api.js
@@ -8,11 +8,13 @@ function getRequest(event) {
   .then((response) => response.json())
   .then(function(data) {
     if(!movieId) {
+      document.getElementById("results").innerHTML = '';
       for(var i in data) {
         document.getElementById("results").innerHTML += data[i].movieTitle + '<br />';
       }
     } else {
       console.log('movieId: ', movieId);
+      document.getElementById("results").innerHTML = '';
       document.getElementById("results").innerHTML += data.movieTitle + '<br />';
     }
     console.log('data:', data);
@@ -45,12 +47,12 @@ function newPost(event) {
 
 
 var deleteButton = document.getElementById('user_form_delete');
-deleteButton.addEventListener('submit', deletePost)
+deleteButton.addEventListener('submit', deletePost);
 
 function deletePost(event) {
   event.preventDefault();
   var movieId = event.target.movieId.value;
-  console.log(movieId);
+  console.log('movie: ', movieId);
   const options = {
     method: 'DELETE',
     headers: new Headers({
@@ -61,26 +63,24 @@ function deletePost(event) {
     })
   }
   const URL = `/movies/${movieId}`;
+
   fetch(URL, options)
   .then(response => response.json())
   .then(data => console.log('movie to delete: ', data))
 }
 
-
 var putButton = document.getElementById('user_form_put');
-putButton.addEventListener('submit', putPost)
+putButton.addEventListener('submit', putPost);
 
 function putPost(event) {
   event.preventDefault();
-  var movieTitle = event.target.movieTitle.value;
-  var movieDirector = event.target.movieDirector.value;
   var movieId = event.target.movieId.value;
-
+  var movieDirector = event.target.movieDirector.value;
+  var movieTitle = event.target.movieTitle.value;
   post = {
     movieTitle: movieTitle,
     movieDirector: movieDirector
   }
-  console.log('post', post);
   const options = {
     method: 'PATCH',
     body: JSON.stringify(post),
@@ -88,7 +88,45 @@ function putPost(event) {
       'Content-Type': 'application/json'
     })
   }
-  return fetch(`/movies/${movieId}`, options)
+  const URL = `/movies/${movieId}`;
+
+  return fetch(URL, options)
   .then(response => response.json())
   .then(data => console.log('movie to update: ', data))
 }
+
+
+
+
+
+
+
+
+
+
+
+// var putButton = document.getElementById('user_form_put');
+// putButton.addEventListener('submit', putPost)
+//
+// function putPost(event) {
+//   event.preventDefault();
+//   var movieTitle = event.target.movieTitle.value;
+//   var movieDirector = event.target.movieDirector.value;
+//   var movieId = event.target.movieId.value;
+//
+//   post = {
+//     movieTitle: movieTitle,
+//     movieDirector: movieDirector
+//   }
+//   console.log('post', post);
+//   const options = {
+//     method: 'PATCH',
+//     body: JSON.stringify(post),
+//     headers: new Headers({
+//       'Content-Type': 'application/json'
+//     })
+//   }
+//   return fetch(`/movies/${movieId}`, options)
+//   .then(response => response.json())
+//   .then(data => console.log('movie to update: ', data))
+// }
